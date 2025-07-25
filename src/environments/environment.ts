08d1f859ab0df0cc5c595c099404245683d791e0
Alejandro Iglesias Raldes <air5795@gmail.com>
2025-07-25 12:13:13 -0400

    aa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,7 +20,7 @@
 };   */
 
 
- export const environment = {
+/*   export const environment = {
   production: false,
   url: "http://10.0.0.152:4001/api/v1/",
   sistema: 47,
@@ -33,5 +33,22 @@
   urlMSAuth: 'http://10.0.0.152:3000/api/auth/get-session',
   urlMSAuthToken: 'http://10.0.0.152:3000/api/auth',
   login: 'http://10.0.0.152:3999/',
-};  
+};   
+ */
+ export const environment = {
+  production: false,
+  url: "http://10.0.10.218:4001/api/v1/",
+  sistema: 47,
+  url_seguridad: "http://10.0.10.218:3009/api/v1/",
+  loginUrlAsus: 'https://test.asuss.gob.bo/asuss-asegurado-api/api/v1/login',
+  consultaUrlAsus:
+    'https://test.asuss.gob.bo/asuss-asegurado-api/api/v2/interoperabilidad/segip/consultaDatosPersona',
+  userNameAsus: 'cbes',
+  passwordAsus: 'cbes.2022**', 
+
+  /* APP SEGURIDAD */
+   urlMSAuth: 'http://10.0.10.217:3000/api/auth/get-session', 
+  urlMSAuthToken: 'http://10.0.10.217:3000/api/auth',
+  login: 'http://10.0.10.200:4300/',
+};   
    
